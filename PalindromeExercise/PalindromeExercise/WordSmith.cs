215e1ffad49a29b91a94dd6d0e61ca8ef10d3fe2
Thomas Donahue <donahue777@yahoo.com>
2024-03-19 18:47:14 -0400

    Done

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public bool IsAPalindrome(string val)
+        {
+            val = val.ToLower();
+
+            char[] charArray = val.ToCharArray();
+
+            Array.Reverse(charArray);
+
+            string reversed = new string(charArray);
+
+            if(reversed == val)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,23 @@
+using PalindromeExercise;
 using System;
+using System.Linq;
 using Xunit;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("poop", true)]
+        [InlineData("Lump", false)]
+        [InlineData("Peep", true)]
+        public void PalidromeTest(string val, bool expected)
         {
+            WordSmith wordy = new WordSmith();
 
+            bool actual = wordy.IsAPalindrome(val);
+
+            Assert.Equal(expected, actual);
         }
     }
 }
